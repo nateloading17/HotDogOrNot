a2dd6cddcc679e08165d4045d99dfed181963a59
nateloading17 <nathanmilobaez@gmail.com>
2024-07-24 10:24:23 -0400

    Initial Commit

---
+++ SeeFood/SeeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodApp.swift
+//  SeeFood
+//
+//  Created by Nathaniel Baez on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SeeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
